681cf1550a23fb0f9e15ed99188af0e95a6dde5e
starkersawz666 <1540422594@qq.com>
2025-01-25 20:54:29 +0000

    Initial commit with rust and hello comp423

---
+++ hello-comp423/target/debug/deps/hello_comp423-81d58ecd0f1dd552.d
@@ -0,0 +1,5 @@
+/workspaces/comp423-rust-container/hello-comp423/target/debug/deps/hello_comp423-81d58ecd0f1dd552: src/main.rs
+
+/workspaces/comp423-rust-container/hello-comp423/target/debug/deps/hello_comp423-81d58ecd0f1dd552.d: src/main.rs
+
+src/main.rs:
---
+++ hello-comp423/target/debug/hello-comp423.d
@@ -0,0 +1,1 @@
+/workspaces/comp423-rust-container/hello-comp423/target/debug/hello-comp423: /workspaces/comp423-rust-container/hello-comp423/src/main.rs
